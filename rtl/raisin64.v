52b8b29dfb3479642554ce52281387050527f46a
Christopher Parish <christopher.parish@parishcomputers.com>
2018-10-10 19:54:18 -0400

    added simple top file

---
+++ rtl/raisin64.v
@@ -0,0 +1,23 @@
+/*
+ * Raisin64 CPU
+ */
+
+module raisin64 (
+    //# {{clocks|Clocking}}
+    input clk,
+    input rst_n,
+    
+    //# {{data|Memory Interface}}
+    input[63:0] mem_din,
+    output[63:0] mem_dout,
+    output[63:0] mem_add,
+
+    //# {{control|Control Signals}}
+    output mem_addr_valid,
+    output mem_dout_write
+    input mem_din_ready,
+
+    //# {{debug|Debug Signals}}
+    input halt);
+
+endmodule
