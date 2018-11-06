ff3d11719f9af7156f902735f4ee21ad9b75734b
Christopher Parish <christopher.parish@parishcomputers.com>
2018-11-05 21:19:45 -0500

    Minor variable name changes and added register commit table

---
+++ rtl/cpu/schedule/cr_table_reg.v
@@ -0,0 +1,36 @@
+//Raisin64 Instruction Scheduler - Committed Register Table
+//Allows
+
+module cr_table_reg #(
+    parameter NUM_READ_PORTS = 3,
+    ) (
+    //# {{clocks|Clocking}}
+    input clk,
+    input rst_n,
+
+    //# {{data|Register Status}}
+    output reg[63:0] reg_busy,
+
+    //# {{control|Control Signals}}
+    input[6:0] busy_rn,
+    input busy_en,
+
+    input[6:0] free_rn[0:NUM_READ_PORTS-1],
+    input free_en[0:NUM_READ_PORTS-1]
+    );
+
+    integer i;
+
+    always @(posedge clk or negedge rst_n)
+    begin
+        if(~rst_n) reg_busy <= 64'h0;
+        else for(i = 0; i < NUM_READ_PORTS; i=i+1)
+        begin
+            if(busy_en) reg_busy[busy_rn] <= 1;
+            else if(free_en[i]) reg_busy[free_rn[i]] <= 0;
+
+            if(busy_en & free_en[i]) $warning("Register in commit table freed and taken simultaneously");
+        end
+    end
+
+endmodule
