bec43053ef751b5aecf2946bc305bb1b5562c622
Christopher Parish <christopher.parish@parishcomputers.com>
2018-11-17 18:08:21 -0500

    Added a filter so GTKWave can convert opcodes into names

--- support/assemble.sh
+++ support/assemble.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-export RAISIN64_BINUTILS=/tmp/staging/bin
-export RAISIN64_CROSS=raisin64-elf
-$RAISIN64_BINUTILS/$RAISIN64_CROSS-as $1 -o prog.elf &&
-$RAISIN64_BINUTILS/$RAISIN64_CROSS-objdump -s -j .data prog.elf &&
-$RAISIN64_BINUTILS/$RAISIN64_CROSS-objdump -d -j .text prog.elf &&
-$RAISIN64_BINUTILS/$RAISIN64_CROSS-objcopy -O binary -j .text prog.elf imem.bin &&
-$RAISIN64_BINUTILS/$RAISIN64_CROSS-objcopy -O binary -j .data prog.elf dmem.bin &&
+raisin64-elf-as $1 -o prog.elf &&
+raisin64-elf-objdump -s -j .data prog.elf &&
+raisin64-elf-objdump -d -j .text prog.elf &&
+raisin64-elf-objcopy -O binary -j .text prog.elf imem.bin &&
+raisin64-elf-objcopy -O binary -j .data prog.elf dmem.bin &&
 xxd -c 1 -ps imem.bin > imem.hex &&
 xxd -c 1 -ps dmem.bin > dmem.hex
