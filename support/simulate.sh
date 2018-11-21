a6852adbfe1c3204010758b6d9c717f7ad31c2de
Christopher Parish <christopher.parish@parishcomputers.com>
2018-11-20 20:08:26 -0500

    Turned up the warnings on icarus

--- support/simulate.sh
+++ support/simulate.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-iverilog -o $1.vpp $1 ../rtl/**/*.v -I../rtl/include &&
+iverilog -Wall -Winfloop -Wno-timescale -Wno-implicit-dimensions -o $1.vpp $1 ../rtl/**/*.v -I../rtl/include &&
 vvp $1.vpp -fst
 rm -f $1.vpp
