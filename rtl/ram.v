fa99c5d9c7aefc7456e1b22f0d287a86a9c95ffe
Christopher Parish <christopher.parish@parishcomputers.com>
2018-11-26 17:25:12 -0500

    RAM should infer block-RAMs while supporting unaligned byte-oriented operation

--- rtl/ram.v
+++ rtl/ram.v
@@ -6,41 +6,108 @@ module ram(input clk,
             input[63:0] data_in,
             output reg[63:0] data_out);
 
-parameter NUM_BYTES = 0;
-parameter INIT_FILE = "";
+    parameter NUM_BYTES = 0;
+    parameter INIT_FILE = "";
 
-reg[7:0] ram[0:NUM_BYTES-1]; //Nx8 RAM
-always @(posedge clk) begin
-    if(we & cs) begin
-        //Ram accesses in this system are byte-oriented
-        ram[addr] <= data_in[63:56];
-        ram[addr+1] <= data_in[55:48];
-        ram[addr+2] <= data_in[47:40];
-        ram[addr+3] <= data_in[39:32];
-        ram[addr+4] <= data_in[31:24];
-        ram[addr+5] <= data_in[23:16];
-        ram[addr+6] <= data_in[15:8];
-        ram[addr+7] <= data_in[7:0];
+    reg[63:0] ram[0:(NUM_BYTES/8)-1]; //Nx64 RAM
+
+    reg[63:0] ramA_result, ramB_result;
+
+    reg[7:0] weA_b; //Which byte lines are write-enabled
+    reg[7:0] weB_b;
+
+    always @(*) begin
+        weA_b = 8'h00;
+        weB_b = 8'h00;
+        if(we) begin
+            case(addr[2:0])
+            3'h0: begin
+                weA_b = 8'hFF;
+                weB_b = 8'h00;
+                end
+
+            3'h1: begin
+                weA_b = 8'h7F;
+                weB_b = 8'h01;
+                end
+
+            3'h2: begin
+                weA_b = 8'h3F;
+                weB_b = 8'h03;
+                end
+
+            3'h3: begin
+                weA_b = 8'h1F;
+                weB_b = 8'h07;
+                end
+
+            3'h4: begin
+                weA_b = 8'h0F;
+                weB_b = 8'h0F;
+                end
+
+            3'h5: begin
+                weA_b = 8'h07;
+                weB_b = 8'h1F;
+                end
+
+            3'h6: begin
+                weA_b = 8'h03;
+                weB_b = 8'h3F;
+                end
+
+            3'h7: begin
+                weA_b = 8'h01;
+                weB_b = 8'h7F;
+                end
+            endcase
+        end
     end
-end
 
-//Registered read (ready by next clock cycle)
-always @(posedge clk) begin
-    if(cs) begin
-        data_out[63:56] <= ram[addr];
-        data_out[55:48] <= ram[addr+1];
-        data_out[47:40] <= ram[addr+2];
-        data_out[39:32] <= ram[addr+3];
-        data_out[31:24] <= ram[addr+4];
-        data_out[23:16] <= ram[addr+5];
-        data_out[15:8] <= ram[addr+6];
-        data_out[7:0] <= ram[addr+7];
-    end else data_out <= 64'h0;
-end
+    generate
+    genvar i;
+    for(i = 0; i < 8; i = i+1) begin
+        always @(posedge clk)
+            if(cs)
+                if(weA_b[i]) begin
+                    ram[addr[63:3]][(i+1)*8-1:i*8] <= data_in[(i+1)*8-1:i*8];
+                end
+        end
 
-//Populate our program memory with the user-provided hex file
-initial begin
-    $readmemh(INIT_FILE, ram);
-end
+    for(i = 0; i < 8; i = i+1) begin
+        always @(posedge clk)
+            if(cs)
+                if(weB_b[i]) begin
+                    ram[addr[63:3]+1][(i+1)*8-1:i*8] <= data_in[(i+1)*8-1:i*8];
+                end
+        end
+    endgenerate
+
+    //Registered read (ready by next clock cycle)
+    always @(posedge clk) begin
+        if(cs) begin
+            ramA_result <= ram[addr[63:3]];
+            ramB_result <= ram[addr[63:3]+1];
+        end
+    end
+
+    always @(*) begin
+        case(addr[2:0])
+        3'h0: data_out = ramA_result;
+        3'h1: data_out = {ramA_result[63:8],ramB_result[63:56]};
+        3'h2: data_out = {ramA_result[63:16],ramB_result[63:48]};
+        3'h3: data_out = {ramA_result[63:24],ramB_result[63:40]};
+        3'h4: data_out = {ramA_result[63:32],ramB_result[63:32]};
+        3'h5: data_out = {ramA_result[63:40],ramB_result[63:24]};
+        3'h6: data_out = {ramA_result[63:48],ramB_result[63:16]};
+        3'h7: data_out = {ramA_result[63:56],ramB_result[63:8]};
+        endcase
+    end
+
+    //Populate our program memory with the user-provided hex file
+    initial begin
+        $readmemh(INIT_FILE, ram);
+    end
+
 
 endmodule
--- support/assemble.sh
+++ support/assemble.sh
@@ -4,5 +4,5 @@ raisin64-elf-objdump -s -j .data prog.elf &&
 raisin64-elf-objdump -d -j .text prog.elf &&
 raisin64-elf-objcopy -O binary -j .text prog.elf imem.bin &&
 raisin64-elf-objcopy -O binary -j .data prog.elf dmem.bin &&
-xxd -c 1 -ps imem.bin > imem.hex &&
-xxd -c 1 -ps dmem.bin > dmem.hex
+xxd -c 8 -ps imem.bin > imem.hex &&
+xxd -c 8 -ps dmem.bin > dmem.hex
